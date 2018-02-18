131d78cd15376ac3843b9c8cb2cf2d3189b56137
Tianjigor <541878782@qq.com>
2018-02-18 23:13:04 +0800

    10-3 在视图中显示模型验证错误

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,6 +1,8 @@
 #-*- coding:utf-8 -*-
 from .base import FunctionalTest
+from selenium.webdriver.common.keys import Keys
 from unittest import skip
+import time
 
 class ItemValidationTest(FunctionalTest):
 
@@ -9,21 +11,25 @@ class ItemValidationTest(FunctionalTest):
         # 伊迪丝访问首页，不小心提交了一个空待办事项
         # 输入框中没输入内容，她就按下了回车键
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         # 首页刷新了，显示一个错误消息
         # 提示待办事项不能为空
-        error = self.browser.find_element_by_css_selector('.has-error')  # ➊
+        error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
         # 她输入一些文字，然后再次提交，这次没问题了
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
-        self.check_for_row_in_list_table('1: Buy milk')  # ➋
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy milk')
         # 她有点儿调皮，又提交了一个空待办事项
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         # 在列表页面她看到了一个类似的错误消息
         self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
         # 输入文字之后就没问题了
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from lists.models import Item, List
+from django.utils.html import escape
 from django.template.loader import render_to_string
 # Create your tests here.
 
@@ -91,6 +92,20 @@ class NewListTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/%d/' % (new_list.id,))
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class NewItemTest(TestCase):
