f529b182aa8c36be66c36949bbd046191bcd7cf8
Tianjigor <541878782@qq.com>
2018-02-18 23:35:40 +0800

    10.4.1 重构：把new_item实现的功能移到view_list中

--- lists/urls.py
+++ lists/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from lists.views import home_page, view_list, new_list, add_item
+from lists.views import home_page, view_list, new_list
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^(\d+)/$', view_list, name='view_list'),
-    url(r'^(\d+)/add_item$', add_item, name='add_item'),
     url(r'^new$', new_list, name='new_list'),
     # url(r'^admin/', admin.site.urls),
 ]
