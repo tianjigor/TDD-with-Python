8e1ed09cc2ce56ec4163fa9a79f9dcf0c912d4d7
Tianjigor <541878782@qq.com>
2018-02-19 17:28:52 +0800

    12.3　处理唯一性验证的复杂表单 err

--- lists/forms.py
+++ lists/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from lists.models import Item
 
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 EMPTY_LIST_ERROR = "You can't have an empty list item"
 # class ItemForm(forms.Form):
 #     item_text = forms.CharField(
@@ -29,3 +30,16 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_LIST_ERROR}
         }
 
+
+class ExistingListItemForm(ItemForm):
+
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValueError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors()
