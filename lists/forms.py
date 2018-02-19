e9d0ff0e7036afe3bf5d4b85ed87c2ed694a4a53
Tianjigor <541878782@qq.com>
2018-02-19 15:54:19 +0800

    11.5　使用表单自带的save方法

--- lists/forms.py
+++ lists/forms.py
@@ -12,6 +12,10 @@ EMPTY_LIST_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
 
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
     class Meta:
         model = Item
         fields = ('text',)
@@ -24,3 +28,4 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_LIST_ERROR}
         }
+
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from lists.models import Item, List
 from lists.forms import ItemForm, EMPTY_LIST_ERROR
 
 class ItemFormTest(TestCase):
@@ -19,3 +19,11 @@ class ItemFormTest(TestCase):
 
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
