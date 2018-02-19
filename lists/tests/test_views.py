2c1275ca32ec3f304b68da526096c54a5187569e
Tianjigor <541878782@qq.com>
2018-02-19 12:40:01 +0800

    11.2.1 在处理GET请求的视图中使用这个表单

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,52 +1,26 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
-from django.http import HttpRequest
 from lists.models import Item, List
+from lists.forms import ItemForm
 from django.utils.html import escape
+from django.http import HttpRequest
 from django.template.loader import render_to_string
 # Create your tests here.
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
-    # def test_home_page_returns_correct_html(self):
-    #     request = HttpRequest()
-    #     response = home_page(request)
-    #     expected_html = render_to_string('home.html',
-    #                                      {'new_item_text': 'A new list item'})
-    #     print(expected_html)
-    #     print(response.content.decode())
-    #     self.assertEqual(response.content.decode(), expected_html)
+    maxDiff = None
 
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
+    def test_home_page_renders_home_template(self):
+        response = self.client.get('/')
 
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
+        self.assertTemplateUsed(response, 'home.html')
 
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
 
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class ListViewTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from lists.models import Item, List
+from lists.forms import ItemForm
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 # Create your views here.
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
