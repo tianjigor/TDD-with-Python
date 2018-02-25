5fcd96bd570ac430639a1f86477baab58310249d
Tianjigor <541878782@qq.com>
2018-02-25 18:09:54 +0800

    19.3. The Python Mock Library

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,48 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
-
+from django.contrib import messages
+from accounts.models import Token
+from django.core.urlresolvers import reverse
 # Create your views here.
+
 def send_login_email(request):
     email = request.POST['email']
-    print (type(send_mail))
+    Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(Token.uid)
+    )
+    message_body = 'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        message_body,
         'noreply@superlists',
-        [email],
+        [email]
     )
+
+    # print (type(send_mail))
+    #
+    # send_mail(
+    #     'Your login link for Superlists',
+    #     'Use this link to log in',
+    #     'noreply@superlists',
+    #     [email],
+    # )
+
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+
+    # messages.add_message(
+    #     request,
+    #     messages.SUCCESS,
+    #     "Check your email, we've sent you a link you can use to log in."
+    # )
+
     return redirect('/')
+
+
+def login(request):
+    return redirect('/')
+
+
