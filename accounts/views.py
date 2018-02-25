e5eb076a86b042c3938ab74c4ef25fe6fd49790c
Tianjigor <541878782@qq.com>
2018-02-25 18:25:27 +0800

    19.3 send_mail test

--- accounts/views.py
+++ accounts/views.py
@@ -7,11 +7,11 @@ from django.core.urlresolvers import reverse
 
 def send_login_email(request):
     email = request.POST['email']
-    Token.objects.create(email=email)
+    token = Token.objects.create(email=email)
     url = request.build_absolute_uri(
-        reverse('login') + '?token=' + str(Token.uid)
+        reverse('login') + '?token=' + str(token.uid)
     )
-    message_body = 'Use this link to log in:\n\n{url}'
+    message_body = 'Use this link to log in:\n\n{}'.format(url)
     send_mail(
         'Your login link for Superlists',
         message_body,
