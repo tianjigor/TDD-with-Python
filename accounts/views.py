6f9c105b58125fb6bd7247163a9d5a9703a258d9
Tianjigor <541878782@qq.com>
2018-02-25 17:11:12 +0800

    19.3. The Python Mock Library

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail
 # Create your views here.
 def send_login_email(request):
     email = request.POST['email']
+    print (type(send_mail))
     send_mail(
         'Your login link for Superlists',
         'body text tbc',
